f5da170a55409a4a0b9b187e7a1fe4fc346498b9
gguram <minkyue.shim@qt.io>
2023-09-12 01:21:37 +0900

    Square Mode & Dark Mode

---
+++ app_window/webp_window.py
@@ -0,0 +1,330 @@
+import os
+import platform
+import sys
+import pathlib
+import webp
+
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
+
+if platform.system() == "Windows":
+    form = os.path.join(os.getcwd(), "WebPConverterGUI.ui")
+else:
+    # build 완료된 exec 에서는 실행이 되지만, 단순 py 로 실행할때는 라이브러리 경로를 참조함
+    form = os.path.join(os.path.dirname(sys.executable), "WebPConverterGUI.ui")
+
+try:
+    formClass = uic.loadUiType(form)[0]
+except:
+    formClass = uic.loadUiType(os.path.join(os.getcwd(), "WebPConverterGUI.ui"))[0]
+
+class WebpApp:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.window = WebpWindow()
+
+    def show(self):
+        self.window.show()
+
+    def exec(self):
+        self.app.exec_()
+
+
+class WebpWindow(QMainWindow, formClass):
+    default_font = 'Barlow-Light'
+
+    def __init__(self):
+        super().__init__()
+
+        self.FontComboBox = None
+        self.listWidget = None
+        self.actionClear_List = None
+
+        self.__selected_font = None
+        self.font_index = 0
+        self.watermakr_option = None
+        self.watermark_text = None
+        self.converter = webp.Converter()
+        self.file_name = []
+
+        self.loseless_option = False
+        self.image_quality_option = 80
+        self.exif_option = False
+        self.icc_profile_option = False
+        self.exact_option = False
+
+        self.conversion_option = True  # webp 변환하는지 선택
+
+        # Exif Options 관련 변수
+        self.exif_padding_option = False  # Exif Padding 을 enable 할 지에 대한 변수
+        self.square_mode_option = False
+        self.dark_mode_option = False
+        self.save_format_index = 0  # JPG, PNG, WebP 파일 형식중 고른 값에 대한 변수
+
+        # self.watermark = []
+
+        self.setupUi(self)
+        self.setup_ui_internal()
+        self.bind_ui()
+        self.init_options()
+
+    def setup_ui_internal(self):
+        if platform.system() == "Windows":
+            font_asset_path = os.path.join(os.getcwd(), "Resources/Fonts")
+        else:
+            font_asset_path = os.path.join(os.path.dirname(sys.executable), "Resources/Fonts")
+
+        print(f"Font_asset: {font_asset_path}")
+        fonts = pathlib.Path(font_asset_path)
+        print(f"Fonts: {fonts}")
+        
+        try:
+            for item in fonts.iterdir():
+                if item.is_file():
+                    continue
+
+                for font_item in os.listdir(item):
+                    print(f"Fonts Item: {font_item}")
+                    self.__add_font_combobox(item, font_item)
+        except:
+            # py 형식으로 실행할 때 macOS 오류 처리용 경로 설정
+            fonts = pathlib.Path(os.path.join(os.getcwd(), "Resources/Fonts"))
+            for item in fonts.iterdir():
+                if item.is_file():
+                    continue
+
+                for font_item in os.listdir(item):
+                    self.__add_font_combobox(item, font_item)
+
+    def __add_font_combobox(self, dir_path, file_name):
+        font_name = os.path.splitext(file_name)[0]
+        item_ext = os.path.splitext(file_name)[1][1:]
+        if item_ext != 'ttf':
+            return
+        fullpath = os.path.join(dir_path, file_name)
+        self.FontComboBox.addItem(font_name, userData=fullpath)
+
+    def bind_ui(self):
+        # 실행 버튼 함수 링킹
+        self.SaveButton.clicked.connect(self.on_click_save)
+        # 파일 추가 버튼 함수 링킹
+        self.actionAdd_Files.triggered.connect(self.on_trigger_add_files)
+        self.actionClear_List.triggered.connect(self.on_trigger_clear_files)
+        # 종료 버튼 함수 링킹
+        self.actionExit.triggered.connect(WebpWindow.on_trigger_exit)
+        # 변환 활성화 trigger 함수 링킹
+        self.ConversionEnableBox.stateChanged.connect(self.on_toggle_conversion_enable)
+        self.ConversionEnableBox.toggle()
+        # Loseless 옵션 링킹
+        self.LoselessOptionBox.stateChanged.connect(self.on_toggle_loseless_option)
+        # 이미지 퀄리티 옵션 링킹
+        self.ImageQualityBox.valueChanged.connect(self.on_change_image_quality)
+        # Exif 정보 저장 옵션 링킹
+        self.ExifOptionBox.stateChanged.connect(self.on_toggle_exif_option)
+        self.ExactOptionBox.stateChanged.connect(self.on_toggle_exact_option)
+        self.ICCProfileOptionBox.stateChanged.connect(self.on_toggle_icc_profile_option)
+        # Watermark 옵션
+        # self.watermarkFontColorBox.stateChanged.connect(self.WatermarkColorOption)
+        # Exif Padding 활성화 옵션 링킹
+        self.EnableExifPadding.stateChanged.connect(self.on_toggle_exif_padding_enable)        
+        self.EnableSquareMode.stateChanged.connect(self.on_change_square_mode)
+        self.EnableDarkMode.stateChanged.connect(self.on_change_dark_mode)
+        self.FontComboBox.currentIndexChanged.connect(self.on_change_font)
+        self.SaveFormatBox.currentIndexChanged.connect(self.on_change_save_format)
+
+    #################### PyQt5 FUNCTIONS
+    def init_options(self):
+        ####################	이미지 품질 관련 옵션
+        self.conversion_option = self.ConversionEnableBox.isChecked()
+        ####################	이미지 품질 관련 옵션
+        self.loseless_option = self.LoselessOptionBox.isChecked()
+        self.image_quality_option = self.ImageQualityBox.value()
+        self.exif_option = self.ExifOptionBox.isChecked()
+        self.icc_profile_option = self.ICCProfileOptionBox.isChecked()
+        self.exact_option = self.ExactOptionBox.isChecked()
+        ####################	워터마크 관련 옵션
+        # self.watermark = self.watermarkBox.toPlainText()
+        # self.watermarkFontColor = self.watermarkFontColorBox.isChecked()
+        ####################	하단 EXIF 삽입 관련 옵션
+        self.exif_padding_option = self.EnableExifPadding.isChecked()
+        self.square_mode_option = self.EnableSquareMode.isChecked()
+        self.dark_mode_option = self.EnableDarkMode.isChecked()
+        self.save_format_index = self.SaveFormatBox.currentIndex()
+        self.font_index = self.FontComboBox.currentIndex()
+        default_font_index = self.FontComboBox.findText(WebpWindow.default_font)
+        self.FontComboBox.setCurrentIndex(default_font_index)
+        self.__selected_font = self.FontComboBox.itemData(self.font_index)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+
+            # 드래그 드롭시 파일 추가되는 코드
+            for url in event.mimeData().urls():
+                self.load_file(url.toLocalFile())
+
+        else:
+            event.ignore()
+
+    def on_click_save(self):
+        if self.listWidget.count() != 0:
+            self.save_file()
+
+    def on_trigger_add_files(self):
+        file_name_list = QFileDialog.getOpenFileNames(self, 'Open Files...', './')
+        if file_name_list is None:
+            print("아무 파일 선택 안됨")
+        else:
+            for name in file_name_list[0]:
+                self.load_file(name)
+
+    def on_trigger_clear_files(self):
+        self.listWidget.clear()
+
+    @staticmethod
+    def on_trigger_exit():
+        sys.exit()
+
+    def watermark_option(self):
+        self.watermark_text = self.watermarkBox.toPlainText()
+        print(self.watermark_text)
+
+    #################### FUNCTIONS
+    def load_file(self, filePath):
+        # ISSUE: 파일 로딩할때 특정 이미지 파일이 누워서 로딩됨 / 혹은 저장할때?
+        icon = QtGui.QIcon(filePath)
+        item = QtWidgets.QListWidgetItem(icon, filePath)
+
+        size = QtCore.QSize()
+        size.setHeight(128)
+        size.setWidth(128)
+
+        item.setSizeHint(size)
+        file_name = os.path.splitext(filePath)[0]
+        self.file_name.append(file_name.split(sep='/')[-1])
+        self.listWidget.addItem(item)
+
+    def save_file(self):
+        # 변환 실행 버튼 callback 함수
+        # self.watermarkOption()
+        
+        save_path = QFileDialog.getExistingDirectory(None, 'Save Directory')
+
+        if save_path:
+            # 01 WebP 이미지로만 변환할 때
+            print(self.__selected_font) # macOS exec 빌드시 None
+            if self.conversion_option:
+                for index in range(self.listWidget.count()):
+                    self.converter.convert_image_to_webp(file_path=self.listWidget.item(index).text(), 
+                                                            save_path=save_path + '/',
+                                                            save_name=self.file_name[index], 
+                                                            loseless_option=self.loseless_option,
+                                                            image_quality_option=self.image_quality_option,
+                                                            exif_option=self.exif_option,
+                                                            icc_profile_option=self.icc_profile_option,
+                                                            exact_option=self.exact_option, watermark_text="",
+                                                            exif_view_option=self.exif_padding_option,
+                                                            conversion_option=self.conversion_option,
+                                                            font_path=self.__selected_font)
+
+            # 02 Exif Padding 이미지로만 변환할때
+            elif self.exif_padding_option:
+                for index in range(self.listWidget.count()):
+                    self.converter.convert_exif_image(file_path=self.listWidget.item(index).text(),
+                                                        save_path=save_path + '/',
+                                                        save_name=self.file_name[index],
+                                                        file_format_option=self.save_format_index,
+                                                        font_path=self.__selected_font,                                                        
+                                                        squre_padding_option=self.square_mode_option,
+                                                        dark_theme_option=self.dark_mode_option,
+                                                        )
+
+            else:
+                print("옵션 선택 에러 / 다시 선택해주세요")
+
+            if platform.system() == "Windows":  # Windows
+                os.startfile(save_path)
+            elif platform.system() == "Darwin":  # macOS
+                os.system("open " + '"' + save_path + '"')
+
+            self.listWidget.clear()
+            self.file_name.clear()
+
+
+    def on_toggle_conversion_enable(self, state):
+        self.conversion_option = bool(state == Qt.Checked)
+        self.EnableExifPadding.setChecked(not state)
+
+        print(f"Conversion Pushed, Conversion Opt: {self.conversion_option}")
+        print(f"Conversion Pushed, Exif Padding Opt: {self.exif_padding_option}")
+
+        if self.conversion_option == False:
+            if self.icc_profile_option == True:
+                self.icc_profile_option = False
+                self.ICCProfileOptionBox.toggle()
+
+            if self.loseless_option == True:
+                self.loseless_option = False
+                self.LoselessOptionBox.toggle()
+
+            if self.exif_option == True:
+                self.exif_option = False
+                self.ExifOptionBox.toggle()
+
+            if self.exact_option == True:
+                self.exact_option = False
+                self.ExactOptionBox.toggle()
+            
+
+    def on_toggle_icc_profile_option(self, state):
+        self.icc_profile_option = bool(state == Qt.Checked)
+
+    def on_toggle_loseless_option(self, state):
+        self.loseless_option = bool(state == Qt.Checked)
+
+    def on_change_image_quality(self):
+        self.image_quality_option = self.ImageQualityBox.value()
+
+    def on_toggle_exif_option(self, state):
+        self.exif_option = bool(state == Qt.Checked)
+
+    def on_toggle_exact_option(self, state):
+        self.exact_option = bool(state == Qt.Checked)
+
+    # 워터마크 옵션
+    def WatermarkColorOption(self, state):
+        self.watermakr_option = bool(state == Qt.Checked)
+
+    # Exif Padding 옵션
+    def on_toggle_exif_padding_enable(self, state):
+        self.exif_padding_option = bool(state == Qt.Checked)
+        self.ConversionEnableBox.setChecked(not state)
+        print(f"Exif Padding Pushed, Conversion Opt: {self.conversion_option}")
+        print(f"Exif Padding Pushed, Exif Padding Opt: {self.exif_padding_option}")
+
+    def on_change_square_mode(self, state):
+        self.square_mode_option = bool(state == Qt.Checked)        
+        print(f"Square Mode Pushed, Squre Mode Opt: {self.square_mode_option}")
+    
+    def on_change_dark_mode(self, state):
+        self.dark_mode_option = bool(state == Qt.Checked)        
+        print(f"Dark Mode Pushed, Dark Mode Opt: {self.dark_mode_option}")
+
+    def on_change_save_format(self):
+        self.save_format_index = self.SaveFormatBox.currentIndex()
+
+    def on_change_font(self):
+        self.font_index = self.FontComboBox.currentIndex()
+        print(f"Font Index: {self.font_index}")
+        self.__selected_font = self.FontComboBox.itemData(self.font_index)
+        print(f"Selected Font: {self.__selected_font}")
---
+++ webp/webp_module.py
@@ -0,0 +1,155 @@
+# Copyright 2023 Hyo Jae Jeon (CANU) canu1832@gmail.com
+
+from PIL import Image, ImageDraw, ImageFont, ImageOps, ExifTags
+
+import os
+# import Watermark_module
+import exif_module
+
+
+class Converter:
+    FILE_FORMAT_EXTENSION = {0: 'jpeg', 1: 'png', 2: 'webp'}
+    FILE_FORMAT_QUALITY_PRESET = {0: 100, 1: 100, 2: 92}
+
+    def __init__(self) -> None:
+        # self.watermark = Watermark_module.Watermark()
+        self.exif = exif_module.Exif()
+
+    @staticmethod
+    def fix_orientation(image):
+        exif = image.getexif()
+        new_image = None
+        try:
+            if exif:
+                orientation = exif.get(274)
+
+                if orientation == 3:
+                    new_image = image.rotate(180, expand=True)
+                elif orientation == 6:
+                    new_image = image.rotate(270, expand=True)
+                elif orientation == 8:
+                    new_image = image.rotate(90, expand=True)
+                else:
+                    new_image = image.rotate(0, expand=True)
+        except Exception as e:
+            print(e)
+
+        return new_image
+
+
+    @staticmethod
+    def convert_image_to_webp(file_path, save_path, save_name, loseless_option, image_quality_option,
+                              exif_option, icc_profile_option, exact_option, watermark_text, exif_view_option,
+                              conversion_option, font_path):
+        file_format = Converter.search_file_format(file_path)
+        # note(komastar) : file_format : 'jpg', 'png'...
+        
+        # 01 일반 WebP 형식 Image로 변환할 때
+        if conversion_option:
+            image = Image.open(file_path).convert("RGB")
+            dest = save_path + save_name + ".webp"
+
+            # 여기서 exif 데이터의 특정 값이 존재하지 않으면 바로 실패함 / 옵션을 선택하지 않아도 읽어오기에 무조건적으로 뻗음
+            try:
+                exif_data = image.getexif()
+                print("Get Exif Data")
+            except:
+                print(f'no exif data {save_name}')
+                exif_option = False
+                exif_data = None
+
+            icc_profile = image.info['icc_profile']
+            print("Get Icc profile")
+
+            # image = self.watermark.InsertWatermark(image=image, fontColor=watermarkColor, watermarkText=watermarkText)
+
+            #image = image.convert("RGB")
+            image = Converter.fix_orientation(image)
+            if exif_option:
+                if icc_profile_option:
+                    image.save(dest, format="webp", loseless=loseless_option, quality=image_quality_option,
+                                exif=exif_data, exact=exact_option, icc_profile=icc_profile)
+                else:
+                    image.save(dest, format="webp", loseless=loseless_option, quality=image_quality_option,
+                                exif=exif_data, exact=exact_option)
+            else:
+                if icc_profile_option:
+                    image.save(dest, format="webp", loseless=loseless_option, quality=image_quality_option,
+                                   exact=exact_option, icc_profile=icc_profile)
+                else:
+                    image.save(dest, format="webp", loseless=loseless_option, quality=image_quality_option,
+                                exact=exact_option)
+
+    def convert_exif_image(self, file_path, save_path, save_name, file_format_option, font_path,
+                            squre_padding_option, dark_theme_option):
+        file_format = Converter.search_file_format(file_path)
+        print("Exif Padding Image")
+
+        if file_format == '':
+            print("잘못된 파일 형식 입니다.")
+            return
+
+        # 02 EXIF Padding Image로 변환할 때
+        image = Image.open(file_path)
+
+        font_color = (0,0,0)
+        background_color = (255,255,255)
+
+        if dark_theme_option == True:
+            font_color=(255,255,255)
+            background_color=(0,0,0)
+
+        longer_length = image.width if image.width >= image.height else image.height
+        padding = int(longer_length / 10)
+        half_padding = int(padding * 0.5)
+
+        model_data, exif_data = self.exif.get_exif_data(image)
+
+        horizontalImage = True if image.width>=image.height else False
+        print("Exif Padding Image2")
+        image = Converter.fix_orientation(image)
+        print("Exif Padding Image3")
+
+        if squre_padding_option==True:
+            image = self.exif.set_square_padding(image, gap = 60, color=background_color, horizontalImage= horizontalImage)
+            print("Exif Padding padding")
+            image = self.exif.set_square_text(image, model_data=model_data, exif_data=exif_data, font_path=font_path, color = font_color, horizontalImage= horizontalImage)
+            print("Exif Padding Text")
+        else : 
+            image = self.exif.set_image_padding2(image, top=half_padding, side=half_padding, bottom=padding, color=background_color)
+            image = self.exif.set_image_text(image,model_data=model_data, exif_data=exif_data, length=padding, font_path=font_path, color = font_color)
+
+        print("Exif Padding Image4")
+        # 파일 형식 선택
+        export_extension = Converter.FILE_FORMAT_EXTENSION[file_format_option]
+        export_quality = Converter.FILE_FORMAT_QUALITY_PRESET[file_format_option]
+        fullpath = os.path.join(save_path, save_name) + '.' + export_extension
+        image.save(fullpath, format=export_extension, quality=export_quality)
+
+    @staticmethod
+    def search_file_format(file_path) -> str:
+        file_format = os.path.splitext(file_path)[1][1:]
+        file_format = file_format.lower()
+
+        support_format = {'jpg', 'jpeg', 'png', 'tiff', 'webp'}
+        if file_format in support_format:
+            return file_format
+        return ''
+
+
+def main():
+    conv = Converter()
+
+    conv.convert_image_to_webp(file_path="Error-Test/P2080300.jpg", 
+                                save_path="Error-Test", 
+                                save_name="P2080300",
+                                loseless_option=True,
+                                image_quality_option=80,
+                                exif_option=True,
+                                icc_profile_option=True,
+                                exact_option=True
+                               )
+
+
+if __name__ == "__main__":
+    main()
